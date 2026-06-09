88fe18fecf7134149c71322bb8375d4c37629a4b
afeng <708835747@qq.com>
2026-06-09 22:14:19 +0800

    做新闻列表

--- models/news.py
+++ models/news.py
@@ -2,7 +2,8 @@ from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped
 from sqlalchemy import func
 from sqlalchemy.ext.asyncio import AsyncSession
 from datetime import datetime
-from sqlalchemy import Integer, String, DateTime, func
+from sqlalchemy import Integer, String, DateTime, func, Index
+from typing import Optional
 
 
 class Base(DeclarativeBase):
@@ -21,3 +22,32 @@ class Category(Base):
 
     def __repr__(self):
         return f"<Category(id={self.id}, name={self.name}, sort_order={self.sort_order}>"
+
+
+class News(Base):
+    __tablename__ = "news"
+
+    __table_args__ = (
+        Index("idx_news_category_id", "category_id"),
+        Index("idx_news_publish_time", "publish_time")
+        )
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, comment="新闻ID")
+    title: Mapped[str] = mapped_column(
+        String(255), nullable=False, comment="新闻标题")
+    description: Mapped[Optional[str]] = mapped_column(
+        String(255), nullable=True, comment="新闻描述")
+    content: Mapped[str] = mapped_column(
+        String(255), nullable=False, comment="新闻内容")
+    image: Mapped[Optional[str]] = mapped_column(
+        String(255), nullable=True, comment="新闻图片URL")
+    author: Mapped[Optional[str]] = mapped_column(
+        String(255), nullable=True, comment="作者")
+    category_id: Mapped[int] = mapped_column(
+        Integer, nullable=False, comment="分类ID")
+
+    views: Mapped[int] = mapped_column(Integer, nullable=False, default=0, comment="点击量")
+    publish_time: Mapped[datetime] = mapped_column(DateTime,
+        default=func.now(),  comment="发布时间时间")
+
+    def __repr__(self):
+        return f"<News(id={self.id}, title={self.title}, category_id={self.category_id}, views={self.views}>"
--- routers/news.py
+++ routers/news.py
@@ -3,6 +3,10 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from crud import news
 from models.news import Category
 from config.db_conf import get_db
+from fastapi import Query
+from models.news import News
+
+
 
 router = APIRouter(prefix="/api/news", tags=["news"])
 
@@ -15,4 +19,20 @@ async def get_categories(
         "code": 200,
         "message": "success",
         "data": categories
+    }
+
+@router.get("/list")
+async def get_news_list(
+        db: AsyncSession = Depends(get_db), 
+        category_id: int = Query(..., alias="categoryId"),
+        page: int = Query(default=1, alias="page"), 
+        page_size: int = Query(default=10, alias="pageSize")):
+    news_data = await news.get_news(db, category_id, page, page_size)
+    newslist: list[News] = news_data['list']
+    total: int = news_data['total']
+    has_more: bool = news_data['has_more']
+    return {
+        "code": 200,
+        "message": "success",
+        "data":{'list': newslist, 'total': total, 'has_more': has_more}
     }
