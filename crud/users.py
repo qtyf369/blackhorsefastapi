a78775f662b7a6de3831b333c13e6b52e5f4a888
ken <708835747@qq.com>
2026-06-16 17:29:43 +0800

    1

--- crud/users.py
+++ crud/users.py
@@ -78,11 +78,8 @@ async def update_user(db: AsyncSession, username: str, userdata: UserUpdateReque
     return user
 
 
-async def update_password(db: AsyncSession, username: str, new_password: str, old_password: str):
-    user = await get_user_by_username(db, username)
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="用户不存在")
+async def update_password(db: AsyncSession, user: User, new_password: str, old_password: str):
+
     from utils.security import verify_password
     if not verify_password(old_password, user.password):
         raise HTTPException(
